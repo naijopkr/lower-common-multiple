d73390ab8371e96c804cd8f0be89ec85fe0ac409
Ariel Barcellos <barcellos.ariel@gmail.com>
2020-04-09 13:40:11 -0500

    minor fixes

--- __main__.py
+++ __main__.py
@@ -1,4 +1,4 @@
-from lowest_common_multiple import lowest_common_multiple
+import lowest_common_multiple as lcm
 
 def get_number(message):
     while True:
@@ -15,7 +15,7 @@ def main():
     while True:
         number1 = get_number('Insert the first number: ')
         number2 = get_number('Insert the second number: ')
-        print(lowest_common_multiple(number1, number2))
+        print(lcm.lowest_common_multiple(number1, number2))
         if input('Wanna try again? (Y/n) ').lower() == 'n':
             break
 
